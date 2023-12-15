f5dfb773c452ea7918e5086e82444de8302d891c
konrad.hanus <konrad.hanus@gmail.com>
2023-12-15 21:31:50 +0100

    bla

--- horoscopio/src/App.js
+++ horoscopio/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import horoscopes from './json/horoscopes.json';
 import {
   BrowserRouter as Router,
   Route,
@@ -14,16 +15,92 @@ const HoroscopePage = () => {
   const day = searchParams.get("day");
   const year = searchParams.get("year");
   const month = searchParams.get("month");
+  
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+
+  
+  function getZodiacSign(dayS, monthS) {
+    
+    const day = +dayS;
+    const month = +monthS;
+
+    if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
+      return "Baran";
+    } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
+      return "Byk";
+    } else if ((month === 5 && day >= 21) || (month === 6 && day <= 20)) {
+      return "Bliźnięta";
+    } else if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) {
+      return "Rak";
+    } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
+      return "Lew";
+    } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
+      return "Panna";
+    } else if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) {
+      return "Waga";
+    } else if ((month === 10 && day >= 23) || (month === 11 && day <= 21)) {
+      return "Skorpion";
+    } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
+      return "Strzelec";
+    } else if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
+      return "Koziorożec";
+    } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+      return "Wodnik";
+    } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+      return "Ryby";
+    } else {
+      // Handle invalid input or edge cases
+      return "Nieprawidłowa data";
+    }
+  }
+  
+ 
+  
+  const znakZodiaku = getZodiacSign(day, month);
+
+  
+  function generujHoroskop(dzienRoku, znakZodiaku, plec) {
+    const horoskopy = horoscopes;
+  
+
+const znalezionyHoroskop = horoskopy.find(
+  element => element.znak_zodiaku === znakZodiaku && element.dzien_roku === randomNumber
+);
+    if (znalezionyHoroskop) {
+      return znalezionyHoroskop.horoskop;
+    } else {
+
+      
+      return "Brak horoskopu dla podanych parametrów.";
+    }
+  }
+  
+  // Przykłady użycia:
+  
+  // ... dodaj inne przypadki, aby sprawdzić poprawność dla różnych dni, znaków zodiaku i płci
+  
 
   return (
     <div>
       <h2>Your Horoscope Page</h2>
-      <p>Name: {name}</p>
-      <p>Sex: {sex}</p>
-      <p>
-        Date of Birth: {day}-{month}-{year}
-      </p>
-      Add your horoscope content here
+      
+      
+
+     
+     {generujHoroskop(day, znakZodiaku, sex)}
+      
+      {/* Add your horoscope content here */}
+      {
+      // 1. import the horoscope file
+      // 2. show the first item with horoscope
+      // 3. generate number from 1 tov 1
+
+
+      // 4. based on the date of birth find the type of the zodiac sign
+      // 5. use parameter sex
+      // 6. filter table based on the parameters
+      }
     </div>
   );
 };
@@ -86,8 +163,8 @@ const HoroscopeForm = () => {
           style={selectStyle}
         >
           <option value="">Select your sex</option>
-          <option value="female">Kobieta</option>
-          <option value="male">Mężczyzna</option>
+          <option value="kobieta">Kobieta</option>
+          <option value="mężczyzna">Mężczyzna</option>
         </select>
       </label>
       <label>
