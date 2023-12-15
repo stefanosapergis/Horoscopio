c72981fefa0164a5568e67c72aaf8564ae16a991
Konrad Hanus <konrad.hanus@intive.com>
2023-12-15 19:19:24 +0100

    poprawione

--- horoscopio/src/App.js
+++ horoscopio/src/App.js
@@ -1,8 +1,15 @@
 // File: App.js
 
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, useHistory } from 'react-router-dom';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
 
 const HoroscopePage = ({ name, sex, dob }) => (
   <div>
@@ -14,59 +21,60 @@ const HoroscopePage = ({ name, sex, dob }) => (
   </div>
 );
 
-const App = () => {
-  const [name, setName] = useState('');
-  const [sex, setSex] = useState('');
-  const [dob, setDob] = useState('');
-  const history = useHistory();
+const HoroscopeForm = () => {
+  const [name, setName] = useState("");
+  const [sex, setSex] = useState("");
+  const [dob, setDob] = useState("");
 
   const handleSubmit = () => {
     // Add logic to handle the form submission, if needed
     // In this example, we simply navigate to the HoroscopePage with the provided data
-    history.push(`/horoscope?name=${name}&sex=${sex}&dob=${dob}`);
+    // Using the `Navigate` component from `react-router-dom`
+    return <Navigate to={`/horoscope?name=${name}&sex=${sex}&dob=${dob}`} />;
   };
 
   return (
+    <div className="form-container">
+      <label>
+        Name:
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Sex:
+        <input
+          type="text"
+          placeholder="Enter your sex"
+          value={sex}
+          onChange={(e) => setSex(e.target.value)}
+        />
+      </label>
+      <label>
+        Date of Birth:
+        <input
+          type="text"
+          placeholder="Enter your date of birth"
+          value={dob}
+          onChange={(e) => setDob(e.target.value)}
+        />
+      </label>
+      <button onClick={handleSubmit}>Dalej</button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
     <Router>
       <div className="App">
-        <div className="form-container">
-          <label>
-            Name:
-            <input
-              type="text"
-              placeholder="Enter your name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-          <label>
-            Sex:
-            <input
-              type="text"
-              placeholder="Enter your sex"
-              value={sex}
-              onChange={(e) => setSex(e.target.value)}
-            />
-          </label>
-          <label>
-            Date of Birth:
-            <input
-              type="text"
-              placeholder="Enter your date of birth"
-              value={dob}
-              onChange={(e) => setDob(e.target.value)}
-            />
-          </label>
-          <button onClick={handleSubmit}>
-            <Link to="/horoscope">Dalej</Link>
-          </button>
-        </div>
-
-        <Switch>
-          <Route path="/horoscope">
-            <HoroscopePage name={name} sex={sex} dob={dob} />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<HoroscopeForm />} />
+          <Route path="/horoscope" element={<HoroscopePage />} />
+        </Routes>
       </div>
     </Router>
   );
