462912b46aa9535ce4b1cb136e43f74840294e9f
Konrad Hanus <konrad.hanus@intive.com>
2023-12-15 20:31:42 +0100

    kod

--- horoscopio/src/App.js
+++ horoscopio/src/App.js
@@ -1,36 +1,69 @@
-// File: App.js
-
 import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Routes,
-  Outlet,
-  Navigate,
+  useNavigate,
 } from "react-router-dom";
 import "./App.css";
 
-const HoroscopePage = ({ name, sex, dob }) => (
-  <div>
-    <h2>Your Horoscope Page</h2>
-    <p>Name: {name}</p>
-    <p>Sex: {sex}</p>
-    <p>Date of Birth: {dob}</p>
-    {/* Add your horoscope content here */}
-  </div>
-);
+const HoroscopePage = () => {
+  const searchParams = new URLSearchParams(window.location.search);
+  const name = searchParams.get("name");
+  const sex = searchParams.get("sex");
+  const day = searchParams.get("day");
+  const year = searchParams.get("year");
+  const month = searchParams.get("month");
+
+  return (
+    <div>
+      <h2>Your Horoscope Page</h2>
+      <p>Name: {name}</p>
+      <p>Sex: {sex}</p>
+      <p>
+        Date of Birth: {day}-{month}-{year}
+      </p>
+      Add your horoscope content here
+    </div>
+  );
+};
 
 const HoroscopeForm = () => {
   const [name, setName] = useState("");
   const [sex, setSex] = useState("");
   const [dob, setDob] = useState("");
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
 
-  const handleSubmit = () => {
-    // Add logic to handle the form submission, if needed
-    // In this example, we simply navigate to the HoroscopePage with the provided data
-    // Using the `Navigate` component from `react-router-dom`
-    return <Navigate to={`/horoscope?name=${name}&sex=${sex}&dob=${dob}`} />;
+  const navigate = useNavigate();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const inputStyle = {
+    width: "100%",
+    padding: "10px",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    fontSize: "16px",
+  };
+
+  const selectStyle = {
+    ...inputStyle,
+    height: "40px", // Ustaw wysokość selectów, aby była taka sama jak dla inputów
   };
 
   return (
@@ -42,27 +75,74 @@ const HoroscopeForm = () => {
           placeholder="Enter your name"
           value={name}
           onChange={(e) => setName(e.target.value)}
+          style={inputStyle}
         />
       </label>
       <label>
         Sex:
-        <input
-          type="text"
-          placeholder="Enter your sex"
+        <select
           value={sex}
           onChange={(e) => setSex(e.target.value)}
-        />
+          style={selectStyle}
+        >
+          <option value="">Select your sex</option>
+          <option value="female">Kobieta</option>
+          <option value="male">Mężczyzna</option>
+        </select>
       </label>
       <label>
         Date of Birth:
-        <input
-          type="text"
-          placeholder="Enter your date of birth"
-          value={dob}
-          onChange={(e) => setDob(e.target.value)}
-        />
+        <div>
+          <select
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+            style={selectStyle}
+          >
+            <option value="">Day</option>
+            {Array.from({ length: 31 }, (_, index) => (
+              <option key={index + 1} value={index + 1}>
+                {index + 1}
+              </option>
+            ))}
+          </select>
+
+          <select
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+            style={selectStyle}
+          >
+            <option value="">Month</option>
+            {months.map((month, index) => (
+              <option key={index + 1} value={index + 1}>
+                {month}
+              </option>
+            ))}
+          </select>
+
+          <select
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+            style={selectStyle}
+          >
+            <option value="">Year</option>
+            {Array.from({ length: 105 }, (_, index) => (
+              <option key={index + 1920} value={index + 1920}>
+                {index + 1920}
+              </option>
+            ))}
+          </select>
+        </div>
       </label>
-      <button onClick={handleSubmit}>Dalej</button>
+
+      <button
+        onClick={() =>
+          navigate(
+            `/horoscope?name=${name}&sex=${sex}&day=${day}&month=${month}&year=${year}`
+          )
+        }
+      >
+        Dalej
+      </button>
     </div>
   );
 };
